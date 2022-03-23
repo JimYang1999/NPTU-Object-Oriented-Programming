bc4b62b235f1c0f73fac47d577628df7998f5e6a
承峻 楊 <azxa318258p55@gmail.com>
2022-03-24 01:07:55 +0800

    Update

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -16,8 +16,8 @@ int main()
 	}
 	str = strtemp;
 	/* 使用函式方法
-	str.erase(remove_if(str.begin(), str.end(), ispunct), str.end());  //string& erase (size_t pos = 0, size_t len = npos);
-	str.erase(remove_if(str.begin(), str.end(), isspace), str.end());
+	str.erase(remove_if(str.begin(), str.end(), ispunct), str.end());  //string& erase (size_t pos = 0, size_t len = npos); 移除符號
+	str.erase(remove_if(str.begin(), str.end(), isspace), str.end());  //移除空白
 	*/
 	//因remove_if無法真正刪除元素，只能把要刪除的元素移到容器末並返回要被刪除元素的迭代器，然後通過erase成員函式來真正刪除。
 	//remove_if (ForwardIterator first, ForwardIterator last,UnaryPredicate pred);
