b030f76fe71594ebc2032bdfbbce076b7e3fdd94
承峻 楊 <azxa318258p55@gmail.com>
2022-03-24 00:04:24 +0800

    Update

--- lexico/lexico.cpp
+++ lexico/lexico.cpp
@@ -3,19 +3,39 @@
 using namespace std;
 int main()
 {
-	string str;
-	cout << "Please input a string:" << endl;
-	getline(cin, str);
-	cout << "Capitalizing..." << endl;
+    int n;
+    cout << "How many strings do you want to input? ";
+    cin >> n;
 
-	str[0] = toupper(str[0]);
-	int pos = 0;
-	while (pos != -1)
-	{
-		pos = str.find(' ', pos);
-		str.replace(pos + 1, 1, 1, toupper(str[pos + 1]));  //string& replace (size_t pos,  size_t len,  size_t n, char c);
-		if (pos != -1)pos += 1;
-	}
-	cout << str;
-	return 0;
+    char** str = new char* [n];
+    cin.ignore();
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Input " << i + 1 << ": ";
+        str[i] = new char[21];
+        cin.getline(str[i], 21);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        char* tmp = str[i];
+        while (*tmp) {
+            *tmp = tolower(*tmp);
+            tmp++;
+        }
+    }
+    cout << "Lexicographical Ordering..." << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (strcmp(str[i], str[j]) < 0)
+            {
+                swap(str[i], str[j]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << str[i] << endl;
+    }
 }
