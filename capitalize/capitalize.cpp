9d96880b24e3684e26365099bbfe925338c7bf10
承峻 楊 <azxa318258p55@gmail.com>
2022-03-23 19:20:46 +0800

    加入專案檔案。

---
+++ capitalize/capitalize.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string str;
+	cout << "Please input a string:" << endl;
+	getline(cin, str);
+	cout << "Capitalizing..." << endl;
+	/* 陣列版本
+	str[0]=toupper(str[0]);
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == ' ')
+		{
+			str[i + 1] = toupper(str[i + 1]);
+		}
+		if (i == str.size() - 1)
+		{
+			cout << str << endl;
+		}
+	}
+	*/
+	/* toupper函數版本
+	int pos = 0;
+	while (pos!=-1)
+	{
+		pos = str.find(' ',pos);
+		str.replace(pos+1,1,1, toupper(str[pos+1]));  //string& replace (size_t pos,  size_t len,  size_t n, char c);
+		if (pos!=-1)pos += 1;
+	}
+	*/
+	/*ASCII版本*/
+	if (int(str[0]) >= 97)str[0] = char(int(str[0] - 32));
+	for (int i = 0; i < str.size(); i++)
+	{
+		(int(str[i]) == 32 and int(str[i + 1]) >= 97 and int(str[i + 1]) <= 122) ? str[i + 1] = char(int(str[i + 1]) - 32) : 0;
+	}
+	cout << str;
+	return 0;
+}
