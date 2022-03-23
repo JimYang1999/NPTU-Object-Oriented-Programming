bf2ba0d254768ff07c7978b6e683dc1832c2509c
承峻 楊 <azxa318258p55@gmail.com>
2022-03-24 01:06:12 +0800

    Update

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -6,8 +6,19 @@ int main()
 	string str;
 	cout << "Please input a string (up to 255 characters):";
 	getline(cin, str);
+	string strtemp;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if ((str[i] >= 'a' and str[i] <= 'z') or (str[i] >= 'A' and str[i] <= 'Z'))
+		{
+			strtemp += str[i];
+		}
+	}
+	str = strtemp;
+	/* 使用函式方法
 	str.erase(remove_if(str.begin(), str.end(), ispunct), str.end());  //string& erase (size_t pos = 0, size_t len = npos);
 	str.erase(remove_if(str.begin(), str.end(), isspace), str.end());
+	*/
 	//因remove_if無法真正刪除元素，只能把要刪除的元素移到容器末並返回要被刪除元素的迭代器，然後通過erase成員函式來真正刪除。
 	//remove_if (ForwardIterator first, ForwardIterator last,UnaryPredicate pred);
 	int k = str.size()-1;
