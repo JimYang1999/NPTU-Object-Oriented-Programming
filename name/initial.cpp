5ae2d45349ab40813191cbaadc19da284b791053
承峻 楊 <azxa318258p55@gmail.com>
2022-04-07 21:34:24 +0800

    new

--- name/initial.cpp
+++ name/initial.cpp
@@ -5,10 +5,42 @@ using namespace std;
 #include "name.h"
 char* initial(Name* n)
 {
+	int i = 0;
 	char* last = new char[20];
-	for (int i = 0; i < strlen(n->lastname); i++)
+	for ( i; i <= strlen(n->lastname); i++)
 	{
+		if (i == strlen(n->lastname))
+		{
+			last[i] = ',';
+			break;
+		}
 		last[i] = n->lastname[i];
 	}
+	i++;
+	last[i] = ' ';
+	int flag = 0;
+	for (int k = 0; k < strlen(n->firstname); k++)
+	{
+		if (int(n->firstname[k]) != 32 && flag == 0)
+		{
+			i++;
+			last[i] = n->firstname[k];
+			i++;
+			last[i] = '.';
+			flag = 1;
+		}
+		if (n->firstname[k] == '-')
+		{
+			i++;
+			last[i] = n->firstname[k];
+			i++;
+			last[i] = n->firstname[k + 1];
+			i++;
+			last[i] = '.';
+			break;
+		}
+	}
+	i++;
+	last[i] = '\0';
 	return last;
 }
